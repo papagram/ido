b75d5760a2300aaaa88421ed7b25328369ff93c6
Shinya Miyata <kamiefan@gmail.com>
2016-11-20 22:26:40 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(
+  subject: '打ち合わせ',
+  body: 'トレーニングの打ち合わせ',
+  status: :finished,
+  start_at: Time.zone.local(2016, 11, 15, 11, 0, 0),
+  closing_at: Time.zone.local(2016, 11, 15, 12, 0, 0)
+)
+
+Task.create(
+  subject: 'Railsアプリをプッシュする',
+  body: 'GitHubにプッシュしてトレーナーをコラボレーターに追加',
+  status: :finished,
+  start_at: Time.zone.local(2016, 11, 20, 21, 0, 0),
+  closing_at: Time.zone.local(2016, 11, 20, 22, 0, 0)
+)
+
+Task.create(
+  subject: 'トレーニング終了日',
+  body: '',
+  start_at: Time.zone.local(2016, 11, 21, 0, 0, 0),
+  closing_at: Time.zone.local(2016, 12, 20, 23, 59, 59)
+)
